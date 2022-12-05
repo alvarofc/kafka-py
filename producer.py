5b70eb8b2da4d8783185828ce6ee161ace475efe
Alvaro Fragoso <alvarofragosoc@hotmail.com>
2022-12-05 16:18:27 +0100

    Update producer.py

--- producer.py
+++ producer.py
@@ -8,7 +8,7 @@ my_producer = KafkaProducer(
     bootstrap_servers = ['localhost:9092'],
     value_serializer = lambda x:dumps(x).encode('utf-8')
     )
-
+# We generate random data every 5 seconds and send it to kafka
 while True:
     my_data = {'sr' : round(random.uniform(45, 100), 2), 'rr' : round(random.uniform(16, 30), 2), 't' : round(random.uniform(85, 99), 2), 'lm' : round(random.uniform(4, 19), 2), 'bo' : round(random.uniform(82, 97), 2), 'rem': round(random.uniform(60, 105), 2), 'sr.1' :round(random.uniform(0, 9), 2), 'hr': round(random.uniform(50, 85), 2)}
     my_producer.send('pillow', value = my_data)
